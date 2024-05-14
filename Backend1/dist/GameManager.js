a2375397b86f457020760732cb46139dca0dfa2d
fahadfahim13 <fahadfahim13@gmail.com>
2024-05-14 17:42:42 +0600

    Added Backend

---
+++ Backend1/dist/GameManager.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GameManager = void 0;
+class GameManager {
+    constructor() {
+        this.games = [];
+    }
+    addUser(socket) {
+    }
+    removeUser(socket) {
+    }
+}
+exports.GameManager = GameManager;
---
+++ Backend1/src/GameManager.ts
@@ -0,0 +1,17 @@
+
+import { Game } from "./types";
+
+export class GameManager {
+    private games: Game[];
+    constructor() {
+        this.games = [];
+    }
+
+    addUser(socket: WebSocket) {
+
+    }
+
+    removeUser(socket: WebSocket) {
+        
+    }
+}
